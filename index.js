ae67e8a4db8cb3f10540791ba595ee74a286d68e
immanuel <immanuel.okoth@student.moringaschool.com>
2026-04-13 11:24:39 +0300

    lab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+function combineUsers(...arrays) {
+  // Merge all arrays into one
+  const mergedUsers = arrays.flat();
 
+  // Get today's date
+  const mergeDate = new Date();
 
+  // Return the required object
+  return {
+    users: mergedUsers,
+    merge_date: mergeDate
+  };
+}
+
+// Export the function (important for tests)
+module.exports = combineUsers;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
